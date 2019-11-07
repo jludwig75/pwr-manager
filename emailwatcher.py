3aa0fdb92bfd360b8791ca25b6d87a1fee65323d
Jonathan Ludwig <jr.ludwig@gmail.com>
2019-11-07 18:02:44 +0000

    moved notification email address to settings.json

--- emailwatcher.py
+++ emailwatcher.py
@@ -1,10 +1,12 @@
 from datetime import datetime
 from gmailer import Gmailer
+import logging
 
 class EmailWatcher:
     def __init__(self, gmail_settings_file_name, notification_address):
         self._mailer = Gmailer(gmail_settings_file_name)
         self._notification_address = notification_address
+        logging.info('Email notifications will be sent to %s' % self._notification_address)
     def notify_event(self, event_message):
         self._mailer.send_gmail(self._notification_address,
                                 subject=event_message,
