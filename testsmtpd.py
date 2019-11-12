8ab34359cbba1bed1f79ccc179cb675b517bab3a
Jonathan Ludwig <jr.ludwig@gmail.com>
2019-11-12 01:08:14 +0000

    finished system test

--- testsmtpd.py
+++ testsmtpd.py
@@ -1,22 +1,53 @@
 import logging
 import threading
+import os
+import smtpd, ssl
 from secure_smtpd import SMTPServer, FakeCredentialValidator, LOG_NAME
 
-class SSLSMTPServer(SMTPServer):
-    def init(self):
-        self._messages = []
+class TestCredentialValidator(object):
+    
+    def validate(self, username, password):
+        if username == 'bcoe' and password == 'foobar':
+            return True
+        return False
+
+class SSLSMTPServer(SMTPServer, threading.Thread):
+    def __init__(self, localaddr, remoteaddr, ssl=False, certfile=None, keyfile=None, ssl_version=ssl.PROTOCOL_SSLv23, require_authentication=False, credential_validator=None, maximum_execution_time=30, process_count=5):
+        SMTPServer.__init__(self,
+                                    localaddr,
+                                    remoteaddr,
+                                    ssl=ssl,
+                                    certfile=certfile,
+                                    keyfile=keyfile,
+                                    ssl_version=ssl_version,
+                                    require_authentication=require_authentication,
+                                    credential_validator=credential_validator,
+                                    maximum_execution_time=maximum_execution_time,
+                                    process_count=process_count)
+        threading.Thread.__init__(self)
+        self._message_file_name = 'dummy.msg'
+        self._lock = threading.Lock()
+        with open(self._message_file_name, 'wt') as msg_file:
+            msg_file.truncate(0)
     def process_message(self, peer, mailfrom, rcpttos, message_data):
-        print(message_data)
-        self._messages.append(message_data)
+        self._lock.acquire()
+        message_text = message_data.split('\n\n')[-1]
+        with open(self._message_file_name, 'at') as msg_file:
+            msg_file.write(message_text + '\n')
+        self._lock.release()
     def pop_messages(self):
-        messages = self._messages
-        self._messages = []
+        self._lock.acquire()
+        if os.path.exists(self._message_file_name):
+            with open(self._message_file_name, 'rt') as msg_file:
+                messages = msg_file.readlines()
+            with open(self._message_file_name, 'wt') as msg_file:
+                msg_file.truncate(0)
+        else:
+            messages = []
+        self._lock.release()
         return messages
-    def start(self):
-        self._thread = threading.Thread(target=self.run)
-        self._thread.start()
-    def stop(self):
-        self._thread.join()
+    def thread_func(self):
+        self.run()
 
 if __name__ == '__main__':
     logger = logging.getLogger( LOG_NAME )
