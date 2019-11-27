6e936c351124956d0389ebf842f7c3f2b9d6c1f3
Jonathan Ludwig <jr.ludwig@gmail.com>
2019-11-27 14:54:40 -0700

    moved mailer to submodule

--- emailwatcher.py
+++ emailwatcher.py
@@ -3,8 +3,8 @@ from mailer import Mailer
 import logging
 
 class EmailWatcher:
-    def __init__(self, mail_settings_file_name, notification_address):
-        self._mailer = Mailer(mail_settings_file_name, 'KASA SmartPlug Power Manager')
+    def __init__(self, mail_settings_file_name, notification_address, sender_address):
+        self._mailer = Mailer(mail_settings_file_name, 'KASA SmartPlug Power Manager', sender_address)
         self._notification_address = notification_address
         logging.info('Email notifications will be sent to %s' % self._notification_address)
     def notify_event(self, event_message):
